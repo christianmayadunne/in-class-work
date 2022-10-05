67eeff4c0afdd8c8b73d7a0b678eeb7f1bf35bc6
christianmayadunne <114947008+christianmayadunne@users.noreply.github.com>
2022-10-05 14:05:40 -0400

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,19 @@
+using System;
+class Program {
+  public static void Main (string[] args) {
+    int a; //Declaration
+    int b; //Declaration
+    int c;
+    int d;
+    double e;
+    a = 1; 
+    b = 2; //initialization
+    c = a + b;
+    d = a * b;
+    e = a % b;
+    Console.WriteLine ("Helloooo World");
+    Console.WriteLine (c);
+    Console.WriteLine (d);
+    Console.WriteLine (e);
+  }
+}
